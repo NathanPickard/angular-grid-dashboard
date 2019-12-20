00510db7dbc8119b15a978d8408ebeee5f2d061a
Nathan Pickard <nathan.pickard87@gmail.com>
2019-12-19 18:56:15 -0800

    Updating user docs in cloud firestore

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -44,4 +44,8 @@ export class AuthService {
     // write to Cloud Firestore
     return this.afs.doc(`users/${user.uid}`).set(userProfile);
   }
+
+  updateUserDocument(userProfile: UserProfile) {
+    return this.afs.doc(`users/${userProfile.uid}`).update(userProfile);
+  }
 }
