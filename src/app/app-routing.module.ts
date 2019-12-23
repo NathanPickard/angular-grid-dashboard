b53d4440785788dc24a8c91e32441deb26656215
Nathan Pickard <nathan.pickard87@gmail.com>
2019-12-23 15:00:53 -0800

    Custom routing pipe for either registered users or admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -43,6 +43,16 @@ const redirectLoggedInToProfileOrUsers = () =>
     })
   )
 
+const allowOnlySelfOrAdmin = (next) =>
+  pipe(
+    customClaims,
+    map(claims => {
+      if (claims.length = 0) {
+        return ['']
+      }
+      return next.params.id === claims.user_id || claims.admin;
+    })
+  )
 const routes: Routes = [
   {
     path: '',
@@ -54,7 +64,7 @@ const routes: Routes = [
     path: 'profile/:id',
     component: ProfileComponent,
     canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: onlyAllowSelf }
+    data: { authGuardPipe: allowOnlySelfOrAdmin }
   },
   {
     path: 'users',
