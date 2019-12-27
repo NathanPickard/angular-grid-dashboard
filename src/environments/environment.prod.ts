2d0bcb189aad4d9b2980d1c70953a170d590cf86
Nathan Pickard <nathan.pickard87@gmail.com>
2019-12-26 22:26:07 -0800

    Firebase hosting configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAripVobYJuc-04njpjTZGj1HwfdvT2zNA',
+    authDomain: 'angular-grid-dashboard.firebaseapp.com',
+    databaseURL: 'https://angular-grid-dashboard.firebaseio.com',
+    projectId: 'angular-grid-dashboard',
+    storageBucket: 'angular-grid-dashboard.appspot.com',
+    messagingSenderId: '925472122263'
+  }
 };
