e65f743305ae9ed8f0bfefe245e2a8eb43cedd6b
Nathan Pickard <nathan.pickard87@gmail.com>
2019-12-26 22:09:22 -0800

    Firebase cloud function sends welcome email to newly signed up users

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,39 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = functions.config().gmail.password;
+
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword
+  }
+})
+
+const APP_NAME = 'The Grid Dashboard';
+
+async function sendWelcomeEmail(email, displayName) {
+  const mailOptions = {
+    from: `${APP_NAME} <nathan.devtest@gmail.com>`,
+    to: email
+  }
+
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to the elite team of secret agents`;
+
+  await mailTransport.sendMail(mailOptions);
+
+  console.log('New welcome email sent to: ', email);
+
+  return null;
+}
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
+  const email = user.email;
+  const displayName = user.displayName;
+
+  return sendWelcomeEmail(email, displayName);
+})
